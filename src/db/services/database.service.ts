50a683becec7df474188ad3424c82414ec918d53
StarAppeal <info@starappeal.app>
2024-11-22 16:58:44 +0100

    small refactor

--- src/db/services/database.service.ts
+++ src/db/services/database.service.ts
@@ -1,68 +1,68 @@
 import "dotenv/config";
 
 import * as mongoDB from "mongodb";
-import { ObjectId, ReturnDocument } from "mongodb";
+import {ObjectId, ReturnDocument} from "mongodb";
 import User from "../models/user";
 
 let mongoDb: mongoDB.Db;
 
 async function getDatabase(): Promise<mongoDB.Db> {
-  if (mongoDb) {
+    if (mongoDb) {
+        return mongoDb;
+    }
+    const client = new mongoDB.MongoClient(process.env.DB_CONN_STRING!);
+    await client.connect();
+    mongoDb = client.db(process.env.DB_NAME!);
     return mongoDb;
-  }
-  const client = new mongoDB.MongoClient(process.env.DB_CONN_STRING!);
-  await client.connect();
-  mongoDb = client.db(process.env.DB_NAME!);
-  return mongoDb;
 }
 
 export class UserService {
-  private readonly collection: mongoDB.Collection;
-  private static _instance: UserService;
+    private static _instance: UserService;
+    private readonly collection: mongoDB.Collection;
 
-  private constructor(db: mongoDB.Db) {
-    this.collection = db.collection(process.env.USER_COLLECTION_NAME!);
-  }
-
-  public static async create(): Promise<UserService> {
-    if (this._instance) {
-      return this._instance;
+    private constructor(db: mongoDB.Db) {
+        this.collection = db.collection(process.env.USER_COLLECTION_NAME!);
     }
-    const db = await getDatabase();
-    this._instance = new UserService(db);
-    return this._instance;
-  }
 
-  public async updateUser(id: string, user: User) {
-    return (await this.executeWithExceptionHandling(() => {
-      return this.collection.findOneAndUpdate(
-        { _id: new ObjectId(id) },
-        { $set: user },
-        { returnDocument: ReturnDocument.AFTER },
-      );
-    })) as unknown as User;
-  }
-
-  public async getAllUsers() {
-    return (await this.executeWithExceptionHandling(() => {
-      return this.collection.find().toArray();
-    })) as unknown as User[];
-  }
-
-  async getUserById(id: string) {
-    return (await this.executeWithExceptionHandling(() => {
-      return this.collection.findOne({
-        _id: new ObjectId(id),
-      });
-    })) as unknown as User;
-  }
-
-  private async executeWithExceptionHandling<T>(operation: () => Promise<T>) {
-    try {
-      return await operation();
-    } catch (e) {
-      console.error(e);
-      return null;
+    public static async create(): Promise<UserService> {
+        if (this._instance) {
+            return this._instance;
+        }
+        const db = await getDatabase();
+        this._instance = new UserService(db);
+        return this._instance;
     }
-  }
+
+    public async updateUser(id: string, user: User): Promise<User> {
+        return await this.executeWithExceptionHandling(() => {
+            return this.collection.findOneAndUpdate(
+                {_id: new ObjectId(id)},
+                {$set: user},
+                {returnDocument: ReturnDocument.AFTER}
+            );
+        }) as unknown as User;
+    }
+
+    public async getAllUsers() {
+        return await this.executeWithExceptionHandling(() => {
+            return this.collection.find<User>({}).toArray();
+        });
+    }
+
+    async getUserById(id: string) {
+        return (await this.executeWithExceptionHandling(() => {
+            return this.collection.findOne<User>({
+                _id: new ObjectId(id),
+            });
+        }));
+    }
+
+    private async executeWithExceptionHandling<T>(operation: () => Promise<T>) {
+        try {
+            return await operation();
+        } catch (e) {
+            console.error(e);
+            return null;
+        }
+    }
 }
