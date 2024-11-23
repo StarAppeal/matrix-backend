4a57f5b2256ba7ecb512e1d83ea5dcd2d1e5cb5f
StarAppeal <info@starappeal.app>
2024-11-23 13:54:13 +0100

    small refactor

--- src/db/services/database.service.ts
+++ src/db/services/database.service.ts
@@ -5,7 +5,6 @@ import mongoose from "mongoose";
 export async function connectToDatabase() {
     await mongoose.connect(process.env.DB_CONN_STRING!, {
         dbName: process.env.DB_NAME!,
-        serverApi: {version: '1', strict: true, deprecationErrors: true}
     });
 
     console.log("Connected to MongoDB with Mongoose");
