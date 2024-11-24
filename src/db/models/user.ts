8d951887fa76e320ab7a3d6027d81c07bdb30779
StarAppeal <silkfan11@gmail.com>
2024-11-24 19:11:30 +0100

    listen on GET_STATE messages and send the current matrix state

--- src/db/models/user.ts
+++ src/db/models/user.ts
@@ -8,13 +8,37 @@ export interface IUser {
     name: string,
     uuid: string,
     id: ObjectId,
-    config: UserConfig
+    config: UserConfig,
+    lastState: MatrixState
 }
 
 export interface UserConfig {
     isVisible: boolean,
     canBeModified: boolean,
     isAdmin: boolean
+}
+
+export interface MatrixState {
+    global: {
+        mode: 'image' | 'text' | "idle" | "music" | "clock";
+        brightness: number;
+    };
+    text: {
+        text: string;
+        align: 'left' | 'center' | 'right'; // Annahme: Mögliche Ausrichtungen sind 'left', 'center', 'right'
+        speed: number;
+        size: number;
+        color: [number, number, number]; // RGB-Werte
+    };
+    image: {
+        image: string; // Der Name der Bilddatei
+    };
+    clock: {
+        color: [number, number, number]; // RGB-Werte
+    };
+    music: {
+        fullscreen: boolean;
+    };
 }
 
 const userSchema = new mongoose.Schema<IUser>({
@@ -39,6 +63,58 @@ const userSchema = new mongoose.Schema<IUser>({
             type: Boolean,
             required: true,
         },
+    },
+    lastState: {
+        global: {
+            mode: {
+                type: String,
+                required: true,
+            },
+            brightness: {
+                type: Number,
+                required: true,
+            },
+        },
+        text: {
+            text: {
+                type: String,
+                required: true,
+            },
+            align: {
+                type: String,
+                required: true,
+            },
+            speed: {
+                type: Number,
+                required: true,
+            },
+            size: {
+                type: Number,
+                required: true,
+            },
+            color: {
+                type: [Number],
+                required: true,
+            },
+        },
+        image: {
+            image: {
+                type: String,
+                required: true,
+            },
+        },
+        clock: {
+            color: {
+                type: [Number],
+                required: true,
+            },
+        },
+        music: {
+            fullscreen: {
+                type: Boolean,
+                required: true,
+            }
+        }
     },
 });
 
--- src/utils/websocket/websocketEventHandler.ts
+++ src/utils/websocket/websocketEventHandler.ts
@@ -1,33 +1,41 @@
-import { ExtendedWebSocket } from "../../interfaces/extendedWebsocket";
+import {ExtendedWebSocket} from "../../interfaces/extendedWebsocket";
+import {UserService} from "../../db/services/UserService";
 
 export class WebsocketEventHandler {
-  constructor(private webSocket: ExtendedWebSocket) {}
+    constructor(private webSocket: ExtendedWebSocket) {
+    }
 
-  public enableErrorEvent() {
-    this.webSocket.on("error", console.error);
-  }
+    public enableErrorEvent() {
+        this.webSocket.on("error", console.error);
+    }
 
-  //needed for the heartbeat mechanism
-  public enablePongEvent() {
-    this.webSocket.on("pong", () => {
-      this.webSocket.isAlive = true;
-      console.log("Pong received");
-    });
-  }
+    //needed for the heartbeat mechanism
+    public enablePongEvent() {
+        this.webSocket.on("pong", () => {
+            this.webSocket.isAlive = true;
+            console.log("Pong received");
+        });
+    }
 
-  public enableDisconnectEvent() {
-    this.webSocket.onclose = (event) => {
-      console.log("WebSocket closed:", event.code, event.reason, event.wasClean, event.type);
-      console.log(`User: ${this.webSocket.payload.name} disconnected`);
-    };
-  }
+    public enableDisconnectEvent() {
+        this.webSocket.onclose = (event) => {
+            console.log("WebSocket closed:", event.code, event.reason, event.wasClean, event.type);
+            console.log(`User: ${this.webSocket.payload.name} disconnected`);
+        };
+    }
 
-  public enableMessageEvent() {
-    this.webSocket.on("message", (data) => {
-      const message = data.toString();
-      console.log("Received message:", message);
-      // just echo the message back to the client
-      this.webSocket.send(message);
-    });
-  }
+    public enableMessageEvent() {
+        this.webSocket.on("message", async (data) => {
+            const message = data.toString();
+            console.log("Received message:", message);
+
+            if (message === "GET_STATE") {
+                const userService = await UserService.create();
+
+                const user = await userService.getUserByUUID(this.webSocket.payload._id);
+
+                this.webSocket.send(JSON.stringify(user?.lastState), {binary: false});
+            }
+        });
+    }
 }
