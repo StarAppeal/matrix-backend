bee6821971898c4a59386dc0a4447b23d577fa14
StarAppeal <info@starappeal.app>
2024-12-07 05:12:39 +0100

    add /me endpoint

--- src/rest/restUser.ts
+++ src/rest/restUser.ts
@@ -3,38 +3,44 @@ import {UserService} from "../db/services/db/UserService";
 import {IUser} from "../db/models/user";
 
 export class RestUser {
-  public createRouter() {
-    const router = express.Router();
+    public createRouter() {
+        const router = express.Router();
 
-    router.get("/", async (req, res) => {
-      const userService = await UserService.create();
-      const users = await userService.getAllUsers();
-      res.status(200).send({ users });
-    });
+        router.get("/", async (req, res) => {
+            const userService = await UserService.create();
+            const users = await userService.getAllUsers();
+            res.status(200).send({users});
+        });
 
-    router.get("/:id", async (req, res) => {
-      const userService = await UserService.create();
-      const id = req.params.id;
-      const user = await userService.getUserById(id);
+        router.get("/me", async (req, res) => {
+            const userService = await UserService.create();
+            const user = await userService.getUserByUUID(req.payload.uuid);
+            res.status(200).send(user);
+        });
 
-      user
-        ? res.status(200).send(user)
-        : res
-            .status(404)
-            .send(`Unable to find matching document with id: ${req.params.id}`);
-    });
+        router.get("/:id", async (req, res) => {
+            const userService = await UserService.create();
+            const id = req.params.id;
+            const user = await userService.getUserById(id);
 
-    router.put("/:id", async (req, res) => {
-      const userService = await UserService.create();
-      const id = req.params.id;
-      const user = req.body as IUser;
-      const result = await userService.updateUserById(id, user);
+            user
+                ? res.status(200).send(user)
+                : res
+                    .status(404)
+                    .send(`Unable to find matching document with id: ${req.params.id}`);
+        });
 
-      result
-        ? res.status(200).send(result)
-        : res.status(304).send("Not Modified");
-    });
+        router.put("/:id", async (req, res) => {
+            const userService = await UserService.create();
+            const id = req.params.id;
+            const user = req.body as IUser;
+            const result = await userService.updateUserById(id, user);
 
-    return router;
-  }
+            result
+                ? res.status(200).send(result)
+                : res.status(304).send("Not Modified");
+        });
+
+        return router;
+    }
 }
