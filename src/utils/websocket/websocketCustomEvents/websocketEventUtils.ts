35ea1d8cb41a0855931b46de1c6f3a4af7f22d65
StarAppeal <silkfan11@gmail.com>
2025-09-30 06:40:09 +0200

    feat: Implement event registry for websocket events

--- src/utils/websocket/websocketCustomEvents/websocketEventUtils.ts
+++ src/utils/websocket/websocketCustomEvents/websocketEventUtils.ts
@@ -1,44 +1,20 @@
 import { ExtendedWebSocket } from "../../../interfaces/extendedWebsocket";
-import { GetSettingsEvent } from "./getSettingsEvent";
-import { ErrorEvent } from "./errorEvent";
-import { GetSpotifyUpdatesEvent } from "./getSpotifyUpdatesEvent";
-import { GetStateEvent } from "./getStateEvent";
-import { GetWeatherUpdatesEvent } from "./getWeatherUpdatesEvent";
-import { StopSpotifyUpdatesEvent } from "./stopSpotifyUpdatesEvent";
-import { StopWeatherUpdatesEvent } from "./stopWeatherUpdatesEvent";
-import { UpdateUserSingleEvent } from "./updateUserEvent";
 import { SpotifyPollingService } from "../../../services/spotifyPollingService";
 import { WeatherPollingService } from "../../../services/weatherPollingService";
-import { SingleSpotifyUpdateEvent } from "./singleSpotifyUpdateEvent";
-import { SingleWeatherUpdateEvent } from "./singleWeatherUpdateEvent";
 
-export type WebsocketEvent =
-    | GetStateEvent
-    | GetSettingsEvent
-    | GetSpotifyUpdatesEvent
-    | StopSpotifyUpdatesEvent
-    | GetWeatherUpdatesEvent
-    | StopWeatherUpdatesEvent
-    | UpdateUserSingleEvent
-    | SingleSpotifyUpdateEvent
-    | SingleWeatherUpdateEvent
-    | ErrorEvent;
+import { eventRegistry, WebsocketEvent } from "./eventRegistry";
 
 export function getEventListeners(
     ws: ExtendedWebSocket,
     spotifyPollingService: SpotifyPollingService,
     weatherPollingService: WeatherPollingService
 ): WebsocketEvent[] {
-    return [
-        new GetStateEvent(ws),
-        new GetSettingsEvent(ws),
-        new GetSpotifyUpdatesEvent(ws, spotifyPollingService),
-        new StopSpotifyUpdatesEvent(ws, spotifyPollingService),
-        new GetWeatherUpdatesEvent(ws, weatherPollingService),
-        new StopWeatherUpdatesEvent(ws, weatherPollingService),
-        new UpdateUserSingleEvent(ws),
-        new SingleSpotifyUpdateEvent(ws),
-        new SingleWeatherUpdateEvent(ws),
-        new ErrorEvent(ws),
-    ];
+    const services = {
+        spotifyPollingService,
+        weatherPollingService,
+    };
+
+    return eventRegistry.map((descriptor) => descriptor.factory(ws, services));
 }
+
+export type { WebsocketEvent };
