68a7f6b9cccd893eaa7ae4540ee7548dcf152a03
StarAppeal <info@starappeal.app>
2025-09-30 05:18:29 +0200

    feat: Update ESLint configuration to ignore build and test files

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,14 @@ const tseslint = require("typescript-eslint");
 const eslintConfigPrettier = require("eslint-config-prettier");
 
 module.exports = tseslint.config(
+    {
+        ignores: [
+            "**/dist/", // Ignoriere den Build-Ordner
+            "**/*.config.js", // Ignoriere Konfigurationsdateien
+            "**/*.test.ts", // Ignoriere alle TypeScript-Testdateien
+            "**/*.spec.ts", // Ignoriere alle TypeScript-Spec-Dateien
+        ]
+    },
     eslint.configs.recommended,
     ...tseslint.configs.recommended,
 
