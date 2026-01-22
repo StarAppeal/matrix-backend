4a349599c5dc53e590c558397ea5efe09266bf79
StarAppeal <silkfan11@gmail.com>
2026-01-22 23:29:32 +0100

    feat: implement default state handling in GetStateEvent

--- src/utils/websocket/websocketCustomEvents/getStateEvent.ts
+++ src/utils/websocket/websocketCustomEvents/getStateEvent.ts
@@ -2,17 +2,26 @@ import { CustomWebsocketEvent } from "./customWebsocketEvent";
 import { WebsocketEventType } from "./websocketEventType";
 import logger from "../../../utils/logger";
 
+const DEFAULT_STATE = {
+    global: {
+        mode: "idle",
+        brightness: 100,
+    },
+};
+
 export class GetStateEvent extends CustomWebsocketEvent {
     event = WebsocketEventType.GET_STATE;
 
     handler = async () => {
         logger.debug(`User ${this.ws.payload?.username} requested state information`);
 
+        const lastState = this.ws.user.lastState ? this.ws.user.lastState : DEFAULT_STATE;
+
         // Send state back to client
         this.ws.send(
             JSON.stringify({
                 type: "STATE",
-                payload: this.ws.user.lastState,
+                payload: lastState,
             }),
             { binary: false }
         );
